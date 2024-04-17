e3a25967f957b0e230dcddaef4205c193ea520cb
gabryel329 <gabryel329@gmail.com>
2024-04-17 15:08:03 -0300

    ADD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="utf-8">
-        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">
         <!-- Stylesheets-->
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Kanit:300,400,500,500i,600,900%7CRoboto:400,900">
         <link rel="stylesheet" href="css/bootstrap.css">
