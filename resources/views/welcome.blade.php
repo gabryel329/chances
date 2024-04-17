5361b09fa2a7649ec75d780ee0d4d0249540cc40
gabryel329 <gabryel329@gmail.com>
2024-04-17 14:53:33 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+    <head>
+        <!-- Site Title-->
+        <title>Chances Vitória</title>
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <!-- Stylesheets-->
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Kanit:300,400,500,500i,600,900%7CRoboto:400,900">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/fonts.css">
+        <link rel="stylesheet" href="css/style.css">
+        <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+    </head>
+    <body>
+        <div class="ie-panel"></div>
+        <div class="preloader">
+            <div class="preloader-body">
+                <div class="preloader-item"></div>
+            </div>
+        </div>
+        <!-- Page-->
+        <div class="page">
+        <!-- Page Header-->
+        <header class="section page-header rd-navbar-dark">
+            <!-- RD Navbar-->
+            <div class="rd-navbar-wrap">
+                <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="166px" data-xl-stick-up-offset="166px" data-xxl-stick-up-offset="166px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+                    <div class="rd-navbar-panel">
+                        <!-- RD Navbar Toggle-->
+                        <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-main"><span></span></button>
+                        <!-- RD Navbar Panel-->
+                        <div class="rd-navbar-panel-inner container">
+                            <div class="rd-navbar-collapse rd-navbar-panel-item rd-navbar-panel-item-left">
+                        </div>
+                        <div class="rd-navbar-panel-item rd-navbar-panel-item-right"></div>
+                        <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+                    </div>
+                    <div class="rd-navbar-main">
+                        <div class="rd-navbar-main-top">
+                            <div class="rd-navbar-main-container container">
+                            <!-- RD Navbar Brand-->
+                            <div class="rd-navbar-brand">
+                                
+                            </div>
+                            <!-- RD Navbar List-->
+                            <ul class="rd-navbar-list">
+                                <a class="brand" href="./">
+                                    <img class="brand-logo " src="images/logo-soccer-default-95x126.png" alt="" width="95" height="126"/>
+                                </a>
+                            </ul>
+                            <!-- RD Navbar Search-->
+                            <div class="rd-navbar-search">
+                                <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Quem Somos</a>
+                                </li>
+                                <li class="rd-nav-item"><a class="rd-nav-link">Chances</a>
+                                </li>
+                                <li class="rd-nav-item"><a class="rd-nav-link">Elenco</a>
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- Swiper-->
+        <section class="section swiper-container swiper-slider swiper-classic bg-gray-2" data-loop="true" data-autoplay="4000" data-simulate-touch="false" data-slide-effect="fade">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide text-center" data-slide-bg="images/slider-1-slide-1-1920x671.jpg">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-xl-6">
+                                <div class="swiper-slide-caption">
+                                    <h1 data-caption-animate="fadeInUp" data-caption-delay="100">We play Soccer</h1>
+                                    <h4 data-caption-animate="fadeInUp" data-caption-delay="200">like no one else in the united states</h4><a class="button button-primary" data-caption-animate="fadeInUp" data-caption-delay="300" href="about-us.html">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" data-slide-bg="images/slider-1-slide-2-1920x671.jpg">
+                    <div class="container">
+                        <div class="row justify-content-end">
+                            <div class="col-xl-5">
+                                <div class="swiper-slide-caption">
+                                    <h1 data-caption-animate="fadeInUp" data-caption-delay="100">We Are Pros</h1>
+                                    <h4 data-caption-animate="fadeInUp" data-caption-delay="200">in Everything Concerning Soccer</h4><a class="button button-primary" data-caption-animate="fadeInUp" data-caption-delay="300" href="about-us.html">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" data-slide-bg="images/slider-1-slide-3-1920x671.jpg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-5">
+                                <div class="swiper-slide-caption">
+                                    <h1 data-caption-animate="fadeInUp" data-caption-delay="100">Best Website</h1>
+                                    <h4 data-caption-animate="fadeInUp" data-caption-delay="200">for soccer news, updates, <br class="d-none d-xl-block"> and game results</h4><a class="button button-primary" data-caption-animate="fadeInUp" data-caption-delay="300" href="about-us.html">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-button swiper-button-prev"></div>
+            <div class="swiper-button swiper-button-next"></div>
+            <div class="swiper-pagination"></div>
+        </section>
+
+        <div class="aside-component">
+            <!-- Heading Component-->
+            <article class="heading-component">
+            <div class="heading-component-inner">
+                <h5 class="heading-component-title">Standings
+                </h5>
+            </article>
+            <!-- Table team-->
+            <div class="table-custom-responsive">
+            <table class="table-custom table-standings table-classic">
+                <thead>
+                <tr>
+                    <th colspan="2">Team Position</th>
+                    <th>W</th>
+                    <th>L</th>
+                    <th>PTS</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><span>1</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-sportland-31x41.png" alt="" width="31" height="41"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Sportland</div>
+                        <div class="team-country">USA</div>
+                    </div>
+                    </td>
+                    <td>153</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                <tr>
+                    <td><span>2</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-dream-team-34x34.png" alt="" width="34" height="34"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Dream team</div>
+                        <div class="team-country">Spain</div>
+                    </div>
+                    </td>
+                    <td>120</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                <tr>
+                    <td><span>3</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-real-madrid-28x37.png" alt="" width="28" height="37"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Real Madrid</div>
+                        <div class="team-country">Spain</div>
+                    </div>
+                    </td>
+                    <td>100</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                <tr>
+                    <td><span>4</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-celta-vigo-35x39.png" alt="" width="35" height="39"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Celta Vigo</div>
+                        <div class="team-country">Italy</div>
+                    </div>
+                    </td>
+                    <td>98</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                <tr>
+                    <td><span>5</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-barcelona-30x35.png" alt="" width="30" height="35"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Barcelona</div>
+                        <div class="team-country">Spain</div>
+                    </div>
+                    </td>
+                    <td>98</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                <tr>
+                    <td><span>6</span></td>
+                    <td class="team-inline">
+                    <div class="team-figure"><img src="images/team-bavaria-fc-26x34.png" alt="" width="26" height="34"/>
+                    </div>
+                    <div class="team-title">
+                        <div class="team-name">Bavaria FC</div>
+                        <div class="team-country">Germany</div>
+                    </div>
+                    </td>
+                    <td>98</td>
+                    <td>30</td>
+                    <td>186</td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+      <footer class="section footer-classic footer-classic-dark context-dark">
+        <div class="footer-classic-aside footer-classic-darken">
+          <div class="container">
+            <div class="layout-justify">
+              <!-- Rights-->
+              <p class="rights"><span>Sportland</span><span>&nbsp;&copy;&nbsp;</span><span class="copyright-year"></span><span>.&nbsp;</span><span>Designed by <a href="https://www.templatemonster.com/products/author/zemez/">Zemez.</a></span></p>
+              <nav class="nav-minimal">
+                <ul class="nav-minimal-list">
+                  <li class="active"><a href="index.html">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Statistics</a></li>
+                  <li><a href="#">Team</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Shop</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- Modal Video-->
+      <div class="modal modal-video fade" id="modal1" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" width="560" height="315" data-src="https://www.youtube.com/embed/uSzNA2_y46c"></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
