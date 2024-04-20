56aa5ceb0fedb142f388461073168839efcc8de6
LeKo <leopardorocha@gmail.com>
2024-04-19 22:43:30 -0300

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
                         <div class="container text-center">
                             <div class="row">
                                 <div class="col-6 col-sm-6">
-                                    <div>
+                                    <div style="text-align: left;">
                                         <a href="./">
                                             <img src="images/logo-soccer-default-95x126.png" alt=""
                                                 width="95" height="126" />
