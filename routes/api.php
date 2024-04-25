93f1dc979018558a628cb184edc85206d3d8048e
gabryel329 <gabryel329@gmail.com>
2024-04-25 14:19:31 -0300

    Atualizando Laravel

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,19 @@ use App\Http\Controllers\Api\ChancesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/welcome', [ChancesController::class, 'welcome']);
+Route::get('/', [ChancesController::class, 'welcome']);
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChancesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-// Route::get('/Elenco', [ChancesController::class, 'showTable2']);
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', [ChancesController::class, 'welcome']);
 
 Route::get('/QuemSomos', [ChancesController::class, 'quemsomos']);
 
 Route::get('/Chances', [ChancesController::class, 'chances']);
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
